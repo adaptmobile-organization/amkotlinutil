702d17f99d202186dfcdd41ae4a04dab658bd12b
Alexander Murillo <alexandersalemander@gmail.com>
2017-09-12 16:52:38 +0200

    Moves currentFocus to constructor to allow other views

--- amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/extensions/ActivityExtensions.kt
+++ amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/extensions/ActivityExtensions.kt
@@ -2,14 +2,14 @@ package dk.adaptmobile.amkotlinutil.extensions
 
 import android.app.Activity
 import android.content.Context
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 /**
  * Created by Alex on 12/09/2017.
  */
 
-fun Activity.hideKeyboard(): Boolean {
-    val view = currentFocus
+fun Activity.hideKeyboard(view: View? = currentFocus): Boolean {
     view?.let {
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         return inputMethodManager.hideSoftInputFromWindow(view.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
