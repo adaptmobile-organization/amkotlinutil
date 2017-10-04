c3f7dd973868586a8b929ed12b35158e9aff85c3
Bjarke Severinsen <bjarke@adapt.dk>
2017-10-04 16:38:17 +0200

    Added locale to long dates extensions

--- amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/extensions/LongExtensions.kt
+++ amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/extensions/LongExtensions.kt
@@ -11,10 +11,10 @@ fun Long.toDate(): Date {
     return Date(this)
 }
 
-fun Long.getDate(format: String = "dd-MM-yyyy"): String {
-    return SimpleDateFormat().format(Date(this))
+fun Long.getDate(format: String = "dd-MM-yyyy", locale: Locale = Locale("da", "DK")): String {
+    return SimpleDateFormat(format, locale).format(Date(this))
 }
 
-fun Long.getTime(format: String = "HH:mm"): String {
-    return SimpleDateFormat(format).format(Date(this))
+fun Long.getTime(format: String = "HH:mm", locale: Locale = Locale("da", "DK")): String {
+    return SimpleDateFormat(format, locale).format(Date(this))
 }
