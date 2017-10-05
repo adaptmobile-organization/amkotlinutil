a147c8782fcb028bacdd166e2080175b61695953
Bjarke Severinsen <bjarke@adapt.dk>
2017-10-05 11:44:44 +0200

    Added getcolor and  get drawable extensions on context using contextcompat

--- amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/extensions/ContextExtensions.kt
+++ amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/extensions/ContextExtensions.kt
@@ -1,6 +1,8 @@
 package dk.adaptmobile.amkotlinutil.extensions
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.support.v4.content.ContextCompat
 import android.view.View
 
 /**
@@ -16,3 +18,8 @@ fun Context.hideViews(vararg views: View) = views.forEach { it.visibility = View
  * Shows all the views passed in the arguments
  */
 fun Context.showViews(vararg views: View) = views.forEach { it.visibility = View.VISIBLE }
+
+
+fun Context.getColorCompat(id: Int): Int = ContextCompat.getColor(this, id)
+
+fun Context.getDrawableCompat(id: Int): Drawable = ContextCompat.getDrawable(this, id)
