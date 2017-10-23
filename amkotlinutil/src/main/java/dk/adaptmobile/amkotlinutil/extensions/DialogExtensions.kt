b645da15ad49608187c7f97835a56ce3ab99f690
Alexander Murillo <alexandersalemander@gmail.com>
2017-10-23 19:57:49 +0200

    Set minimum version to 16 Updates dependencies and compileSdk

--- amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/extensions/DialogExtensions.kt
+++ amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/extensions/DialogExtensions.kt
@@ -8,31 +8,31 @@ import android.widget.TextView
  * Created by Thomas on 07/06/2017.
  */
 fun Dialog.setCustomTitle(resId: Int, title: String) {
-    val titleView = findViewById(resId) as TextView
+    val titleView: TextView = findViewById(resId)
     titleView.text = title
 }
 
 fun Dialog.setCustomText(resId: Int, message: String) {
-    val messageView = findViewById(resId) as TextView
+    val messageView: TextView = findViewById(resId)
     messageView.text = message
 }
 
 fun Dialog.setConfirmButtonText(resId: Int, text: String) {
-    val confirmButton = findViewById(resId) as TextView
+    val confirmButton: TextView = findViewById(resId)
     confirmButton.text = text
 }
 
 fun Dialog.setCancelButtonText(resId: Int, text: String) {
-    val cancelButton = findViewById(resId) as TextView
+    val cancelButton: TextView = findViewById(resId)
     cancelButton.text = text
 }
 
 fun Dialog.setConfirmButtonOnClick(resId: Int, clickListener: View.OnClickListener) {
-    val confirmButton = findViewById(resId) as TextView
+    val confirmButton: TextView = findViewById(resId)
     confirmButton.setOnClickListener(clickListener)
 }
 
 fun Dialog.setCancelButtonOnClick(resId: Int, clickListener: View.OnClickListener) {
-    val cancelButton = findViewById(resId) as TextView
+    val cancelButton: TextView = findViewById(resId)
     cancelButton.setOnClickListener(clickListener)
 }
