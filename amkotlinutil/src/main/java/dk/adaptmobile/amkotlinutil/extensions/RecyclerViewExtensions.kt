709eb41b3dafb9f4efac258d5a25749332ed2c67
Christian Steffensen <christian.steffensen@adapt.dk>
2018-11-08 13:01:18 +0100

    fixed build

--- amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/extensions/RecyclerViewExtensions.kt
+++ amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/extensions/RecyclerViewExtensions.kt
@@ -7,15 +7,15 @@ val RecyclerView.ViewHolder.context: Context
     get() = this.itemView.context
 
 fun RecyclerView.disableAnimations() {
-    this.itemAnimator.addDuration = 0
-    this.itemAnimator.removeDuration = 0
-    this.itemAnimator.moveDuration = 0
-    this.itemAnimator.changeDuration = 0
+    this.itemAnimator?.addDuration = 0
+    this.itemAnimator?.removeDuration = 0
+    this.itemAnimator?.moveDuration = 0
+    this.itemAnimator?.changeDuration = 0
 }
 
 fun RecyclerView.enableAnimations() {
-    this.itemAnimator.addDuration = 120
-    this.itemAnimator.removeDuration = 120
-    this.itemAnimator.moveDuration = 250
-    this.itemAnimator.changeDuration = 250
+    this.itemAnimator?.addDuration = 120
+    this.itemAnimator?.removeDuration = 120
+    this.itemAnimator?.moveDuration = 250
+    this.itemAnimator?.changeDuration = 250
 }
