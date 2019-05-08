a67feb8b023972b6517e14e7778a13ad078d9df1
Bjarke Severinsen <bjarkeseverinsen@Bjarkes-MBP.lbg.adapt.dk>
2019-05-08 16:29:22 +0200

    Fix

--- amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/extensions/NetworkExtensions.kt
+++ amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/extensions/NetworkExtensions.kt
@@ -3,6 +3,6 @@ package dk.adaptmobile.amkotlinutil.extensions
 import okhttp3.Response
 import okhttp3.ResponseBody
 
-private fun Response.cloneResponseBody(): ResponseBody {
+fun Response.cloneResponseBody(): ResponseBody {
     return ResponseBody.create(this.body()!!.contentType(), this.peekBody(Long.MAX_VALUE).string())
 }
