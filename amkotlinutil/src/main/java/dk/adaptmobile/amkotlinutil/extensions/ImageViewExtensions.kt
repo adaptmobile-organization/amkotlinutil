525e738cd5adc87e4a9d4b1a91ab06a928bd47f9
Christian Steffensen <christian.steffensen@adaptagency.com>
2019-05-15 10:27:54 +0200

    added optional image loaded callback to Glide loadImageResources

--- amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/extensions/ImageViewExtensions.kt
+++ amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/extensions/ImageViewExtensions.kt
@@ -7,7 +7,11 @@ import android.util.Base64
 import android.widget.ImageView
 import androidx.annotation.ColorRes
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 
 /**
  * Created by christiansteffensen on 05/06/2017.
@@ -17,31 +21,31 @@ sealed class Transformation {
     object Circle : Transformation()
 }
 
-fun ImageView.loadImageResource(imageResource: String?, skipMemoryCache: Boolean = false, transformation: Transformation? = null) {
+fun ImageView.loadImageResource(imageResource: String?, skipMemoryCache: Boolean = false, transformation: Transformation? = null, imageLoadedCallback: (() -> Unit)? = null) {
     imageResource?.let {
-        loadImage(it, skipMemoryCache, transformation)
+        loadImage(it, skipMemoryCache, transformation, imageLoadedCallback)
     }
 }
 
-fun ImageView.loadImageResource(imageResource: Int?, skipMemoryCache: Boolean = false, transformation: Transformation? = null) {
+fun ImageView.loadImageResource(imageResource: Int?, skipMemoryCache: Boolean = false, transformation: Transformation? = null, imageLoadedCallback: (() -> Unit)? = null) {
     imageResource?.let {
-        loadImage(it, skipMemoryCache, transformation)
+        loadImage(it, skipMemoryCache, transformation, imageLoadedCallback)
     }
 }
 
-fun ImageView.loadImageResource(imageResource: Bitmap?, skipMemoryCache: Boolean = false, transformation: Transformation? = null) {
+fun ImageView.loadImageResource(imageResource: Bitmap?, skipMemoryCache: Boolean = false, transformation: Transformation? = null, imageLoadedCallback: (() -> Unit)? = null) {
     imageResource?.let {
-        loadImage(it, skipMemoryCache, transformation)
+        loadImage(it, skipMemoryCache, transformation, imageLoadedCallback)
     }
 }
 
-fun ImageView.loadImageResource(imageResource: Drawable?, skipMemoryCache: Boolean = false, transformation: Transformation? = null) {
+fun ImageView.loadImageResource(imageResource: Drawable?, skipMemoryCache: Boolean = false, transformation: Transformation? = null, imageLoadedCallback: (() -> Unit)? = null) {
     imageResource?.let {
-        loadImage(it, skipMemoryCache, transformation)
+        loadImage(it, skipMemoryCache, transformation, imageLoadedCallback)
     }
 }
 
-private fun ImageView.loadImage(imageResource: Any, skipMemoryCache: Boolean, transformation: Transformation?) {
+private fun ImageView.loadImage(imageResource: Any, skipMemoryCache: Boolean, transformation: Transformation?, imageLoadedCallback: (() -> Unit)?) {
     var requestOptions = RequestOptions()
             .skipMemoryCache(skipMemoryCache)
 
@@ -51,34 +55,48 @@ private fun ImageView.loadImage(imageResource: Any, skipMemoryCache: Boolean, tr
         else -> requestOptions // Do nothing
     }
 
-    Glide.with(context)
+
+    val glide = Glide.with(context)
             .load(imageResource)
             .apply(requestOptions)
-            .into(this)
+
+    imageLoadedCallback?.let {
+        glide.addListener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean = false
+
+            override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                imageLoadedCallback()
+                return false
+            }
+
+        })
+    }
+
+    glide.into(this)
 }
 
 @Deprecated("Deprecated, use loadImageResource with parameters instead",
         replaceWith = ReplaceWith("loadImageResource(imageResource, transformation = Transformation.CenterCrop)"))
 fun ImageView.loadImageResourceCenterCrop(imageResource: String?) {
-   loadImageResource(imageResource, transformation =  Transformation.CenterCrop)
+    loadImageResource(imageResource, transformation = Transformation.CenterCrop)
 }
 
 @Deprecated("Deprecated, use loadImageResource with parameters instead",
         replaceWith = ReplaceWith("loadImageResource(imageResource, transformation = Transformation.CenterCrop)"))
 fun ImageView.loadImageResourceCenterCrop(imageResource: Int?) {
-    loadImageResource(imageResource, transformation =  Transformation.CenterCrop)
+    loadImageResource(imageResource, transformation = Transformation.CenterCrop)
 }
 
 @Deprecated("Deprecated, use loadImageResource with parameters instead",
         replaceWith = ReplaceWith("loadImageResource(imageResource, transformation = Transformation.Circle)"))
 fun ImageView.loadImageResourceAsCircle(imageResource: String?) {
-    loadImageResource(imageResource, transformation =  Transformation.Circle)
+    loadImageResource(imageResource, transformation = Transformation.Circle)
 }
 
 @Deprecated("Deprecated, use loadImageResource with parameters instead",
         replaceWith = ReplaceWith("loadImageResource(imageResource, transformation = Transformation.Circle)"))
 fun ImageView.loadImageResourceAsCircle(imageResource: Int?) {
-    loadImageResource(imageResource, transformation =  Transformation.Circle)
+    loadImageResource(imageResource, transformation = Transformation.Circle)
 }
 
 fun ImageView.setTint(@ColorRes colorRes: Int, mode: PorterDuff.Mode = PorterDuff.Mode.SRC_OVER) {
