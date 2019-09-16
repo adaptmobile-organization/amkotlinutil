9a0b8d166f98d73a1f9c35d5461d09f3652c1cd3
Martin L. Jensen <martinloesethjensen@gmail.com>
2019-09-16 14:29:12 +0200

    Added ktlint

--- amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/managers/DownloadManager.kt
+++ amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/managers/DownloadManager.kt
@@ -15,11 +15,8 @@ import androidx.annotation.RequiresPermission
  * @author Jason Kelly <jason@adaptagency.com>
  */
 @SuppressLint("StaticFieldLeak")
-object DownloadManager
-{private lateinit var applicationContext: Context
+object DownloadManager { private lateinit var applicationContext: Context
         private lateinit var downloadManager: DownloadManager
-
-
 
         /**
          * This will initialize the download manager with the specified
