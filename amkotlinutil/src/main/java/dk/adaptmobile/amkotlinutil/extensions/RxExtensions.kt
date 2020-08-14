6b800ed27f9cdee2d746e6bf37c7633073598a99
Gabor Hartyandi <gabor.hartyandi@gmail.com>
2020-08-14 15:24:13 +0200

    Remove Rx2 Flowable import

--- amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/extensions/RxExtensions.kt
+++ amkotlinutil/src/main/java/dk/adaptmobile/amkotlinutil/extensions/RxExtensions.kt
@@ -1,7 +1,6 @@
 package dk.adaptmobile.amkotlinutil.extensions
 
 import com.github.ajalt.timberkt.e
-import io.reactivex.Flowable
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
